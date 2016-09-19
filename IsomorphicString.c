9a60b874aa45bda5429932089d81927a39108223
Yunobububu <lvjinkebit@163.com>
2016-09-19 15:19:19 +0800

    Hash Table to find 2 isomorphic strings

--- IsomorphicString.c
+++ IsomorphicString.c
@@ -4,39 +4,47 @@
 
     void main()
     {
-        char *s="AaAff";
-        char *t="CcCDb";
-        int i,j,k,n1,n2;
-        n1=strlen(s);
-        n2=strlen(t);
-        char H1[256]={0};
-        char H2[256]={0};
-        for(k=0;k<n1;k++)
-        {
-            if(H1[s[k]]!=H2[t[k]])
-            {
-                printf("s&t is not isophoic string\n");
-                return -1;
-            }
+        char *s1[20]={0};
+        char *s2[20]={0};
+        printf("please input string 1:\n");
+        gets(s1);
+        printf("please input string 2:\n");
+        gets(s2);
+        IsomorphicString(s1,s2);
 
-            else
-            {
-                H1[s[k]]=k+1;
-                H2[t[k]]=k+1;
-            }
-
-
-
-        }
-        printf("s&t is isophoic string\n");
         return 0;
+    }
+IsomorphicString(char *s1,char *s2)
+{
+    int n1,n2,i,k;
+    char m1[256]={0};
+    char m2[256]={0};
+    n1=strlen(s1);
+    n2=strlen(s2);
 
+    if(n1!=n2)
+    {
+        printf("the length of s&t is not equal\n");
+        return -1;
+    }
 
-
-
-
+    for(i=0;i<n1;i++)
+    {
+        if(m1[s1[i]]!=m2[s2[i]])
+        {
+            printf("s&t is not isomorphic\n");
+            return -1;
+        }
+        else
+        {
+            m1[s1[i]]=i+1;
+            m2[s2[i]]=i+1;
+        }
 
     }
+    printf("s&t is isomorhic\n");
+
+}
 
 
 
