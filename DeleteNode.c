5ac23451a4605408776353766f04acc019593f05
Yunobububu <lvjinkebit@163.com>
2016-09-19 23:13:43 +0800

    DeleteNode
    use struct?

---
+++ DeleteNode.c
@@ -0,0 +1,18 @@
+//delete the Node
+#include<stdio.h>
+void main()
+{
+    struct ListNode{
+
+        int val;
+        struct ListNode *next;
+    }
+
+}
+
+void deleteNode(struct ListNode* node)
+{
+
+
+
+}
